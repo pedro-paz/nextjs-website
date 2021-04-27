755f3b239ba0b8193c47cc04cf925f8d17927e06
Pedro <pg.alves.paz@gmail.com>
2021-04-27 00:31:59 -0300

    title page changed

--- src/components/pageContainer/index.tsx
+++ src/components/pageContainer/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const PageContainer = styled.div`
   font-weight: 800;
-  font-size: 42px;
+  font-size: 38px;
   padding: 33px 55px;
   color: #e2e2e2;
 `;
